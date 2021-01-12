7de6b6a2fe697de250ec1832211f955d3dbf1ed9
Oleh Kravets <oleg.kravets92@gmail.com>
2021-01-12 14:12:46 +0200

    added CarService and DriverService methods

--- src/main/java/core/service/car/CarService.java
+++ src/main/java/core/service/car/CarService.java
@@ -1,7 +1,26 @@
 package core.service.car;
 
 import core.lib.Service;
+import core.model.Car;
+import core.model.Driver;
+import java.util.List;
 
 @Service
 public interface CarService {
+    
+    Car add(Car car);
+    
+    Car get(Long id);
+    
+    List<Car> getAll();
+    
+    Car update(Car car);
+    
+    boolean delete(Long id);
+    
+    void addDriverToCar(Driver driver, Car car);
+    
+    void removeDriverFromCar(Driver driver, Car car);
+    
+    List<Car> getAllByDriver(Long driverId);
 }
--- src/main/java/core/service/driver/DriverService.java
+++ src/main/java/core/service/driver/DriverService.java
@@ -1,7 +1,18 @@
 package core.service.driver;
 
 import core.lib.Service;
+import core.model.Driver;
+import java.util.List;
 
 @Service
 public interface DriverService {
+    Driver create(Driver driver);
+    
+    Driver get(Long id);
+    
+    List<Driver> getAll();
+
+    Driver update(Driver driver);
+    
+    boolean delete(Long id);
 }
