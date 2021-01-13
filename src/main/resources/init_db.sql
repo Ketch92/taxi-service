eefbf67541c2696176ebdfb905bbf43c8ec2ee4a
Oleh Kravets <oleg.kravets92@gmail.com>
2021-01-13 23:10:09 +0200

    updated init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,12 +1,29 @@
-create table manufacturers
-(
-    id      bigserial    not null
-        constraint manufacturers_pk
-            primary key,
-    name    varchar(225) not null,
-    country varchar(225) not null,
-    deleted boolean default false
-);
+CREATE DATABASE taxi_service
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Ukrainian_Ukraine.1251'
+    LC_CTYPE = 'Ukrainian_Ukraine.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
 
-alter table manufacturers
-    owner to postgres;
+CREATE SCHEMA public
+    AUTHORIZATION postgres;
+
+COMMENT ON SCHEMA public
+    IS 'standard public schema';
+
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+GRANT ALL ON SCHEMA public TO postgres;
+
+CREATE TABLE public.manufacturers
+(
+    id bigint NOT NULL DEFAULT nextval('manufacturers_id_seq'::regclass),
+    name character varying(225) COLLATE pg_catalog."default" NOT NULL,
+    country character varying(225) COLLATE pg_catalog."default" NOT NULL,
+    deleted boolean DEFAULT false,
+    CONSTRAINT manufacturers_pk PRIMARY KEY (id)
+)
+
+
