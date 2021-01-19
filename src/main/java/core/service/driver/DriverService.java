e1106c88b1c5a63c73fd0ae6b3bdbd07959c4097
Oleh Kravets <oleg.kravets92@gmail.com>
2021-01-19 15:34:20 +0200

    added generic service

--- src/main/java/core/service/driver/DriverService.java
+++ src/main/java/core/service/driver/DriverService.java
@@ -1,16 +1,8 @@
 package core.service.driver;
 
 import core.model.Driver;
+import core.service.Service;
 import java.util.List;
 
-public interface DriverService {
-    Driver add(Driver driver);
-    
-    Driver get(Long id);
-    
-    List<Driver> getAll();
-
-    Driver update(Driver driver);
-    
-    boolean delete(Long id);
+public interface DriverService extends Service<Driver, Long> {
 }
