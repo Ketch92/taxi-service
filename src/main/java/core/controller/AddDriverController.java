be555e4ea09edaed93da380d18c268a6a30e7f97
Oleh Kravets <oleg.kravets92@gmail.com>
2021-01-20 15:37:49 +0200

    added and implemented AddDriverController

---
+++ src/main/java/core/controller/AddDriverController.java
@@ -0,0 +1,31 @@
+package core.controller;
+
+import core.Main;
+import core.lib.Injector;
+import core.model.Driver;
+import core.service.driver.DriverService;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddDriverController extends HttpServlet {
+    private static final Injector injector
+            = Injector.getInstance(Main.class.getPackageName());
+    private DriverService driverService
+            = (DriverService) injector.getInstance(DriverService.class);
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/add_driver.jsp").forward(req, resp);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String driverName = req.getParameter("driverName");
+        String driverLicence = req.getParameter("driverLicence");
+        driverService.add(new Driver(driverName, driverLicence));
+        resp.sendRedirect(req.getContextPath() + "/");
+    }
+}
