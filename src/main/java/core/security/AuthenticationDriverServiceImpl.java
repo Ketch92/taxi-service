afee9ef548da73d40581ac1c2b48e488b3adac41
Oleh Kravets <oleg.kravets92@gmail.com>
2021-01-21 23:29:40 +0200

    updated AuthenticationDriverServiceImpl

--- src/main/java/core/security/AuthenticationDriverServiceImpl.java
+++ src/main/java/core/security/AuthenticationDriverServiceImpl.java
@@ -1,21 +1,21 @@
 package core.security;
 
+import core.dao.driver.DriverDao;
 import core.lib.Inject;
 import core.lib.Service;
 import core.model.Driver;
 import core.model.exception.AuthenticationException;
-import core.service.driver.DriverService;
 
 @Service
 public class AuthenticationDriverServiceImpl implements AuthenticationDriverService {
     
     @Inject
-    private DriverService driverService;
+    private DriverDao dao;
     
     @Override
     public Driver login(String login, String password) throws AuthenticationException {
         String message = "No matches for such login or password.";
-        Driver driver = driverService.getByLogin(login)
+        Driver driver = dao.getByLogin(login)
                 .orElseThrow(() -> new AuthenticationException(message));
         if (password.equals(driver.getPassword())) {
             return driver;
