f46ee67db5fe4fbf7202309de91a5f1f190b20ad
Oleh Kravets <oleg.kravets92@gmail.com>
2021-01-22 12:02:44 +0200

    updated init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -31,29 +31,15 @@ SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
--- SCHEMA: public
-
--- DROP SCHEMA public ;
-
-CREATE SCHEMA public
-    AUTHORIZATION postgres;
-
-COMMENT ON SCHEMA public
-    IS 'standard public schema';
-
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
-GRANT ALL ON SCHEMA public TO postgres;
-
 --
 -- Name: cars; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.cars (
-                             id bigint NOT NULL,
-                             model character varying(225) NOT NULL,
-                             manufacturer bigint NOT NULL,
-                             deleted boolean DEFAULT false
+    id bigint NOT NULL,
+    model character varying(225) NOT NULL,
+    manufacturer bigint NOT NULL,
+    deleted boolean DEFAULT false
 );
 
 
@@ -64,8 +50,8 @@ ALTER TABLE public.cars OWNER TO postgres;
 --
 
 CREATE TABLE public.cars_drivers (
-                                     car_id bigint NOT NULL,
-                                     driver_id bigint NOT NULL
+    car_id bigint NOT NULL,
+    driver_id bigint NOT NULL
 );
 
 
@@ -108,10 +94,12 @@ ALTER SEQUENCE public.cars_id_seq OWNED BY public.cars.id;
 --
 
 CREATE TABLE public.drivers (
-                                id bigint NOT NULL,
-                                name character varying(225) NOT NULL,
-                                licence_number character varying(225) NOT NULL,
-                                deleted boolean DEFAULT false
+    id bigint NOT NULL,
+    name character varying(225) NOT NULL,
+    licence_number character varying(225) NOT NULL,
+    deleted boolean DEFAULT false,
+    login character varying(225),
+    password character varying(225)
 );
 
 
@@ -143,10 +131,10 @@ ALTER SEQUENCE public.drivers_id_seq OWNED BY public.drivers.id;
 --
 
 CREATE TABLE public.manufacturers (
-                                      id bigint NOT NULL,
-                                      name character varying(225) NOT NULL,
-                                      country character varying(225) NOT NULL,
-                                      deleted boolean DEFAULT false
+    id bigint NOT NULL,
+    name character varying(225) NOT NULL,
+    country character varying(225) NOT NULL,
+    deleted boolean DEFAULT false
 );
 
 
@@ -199,6 +187,16 @@ ALTER TABLE ONLY public.manufacturers ALTER COLUMN id SET DEFAULT nextval('publi
 --
 
 COPY public.cars (id, model, manufacturer, deleted) FROM stdin;
+1	Any	11	f
+2	Any	2	f
+3	Any	14	f
+4	Any	11	f
+5	Any	1	f
+6	Any	6	f
+7	Any	8	f
+8	Any	9	f
+9	Any	12	f
+10	Any	2	f
 \.
 
 
@@ -206,7 +204,61 @@ COPY public.cars (id, model, manufacturer, deleted) FROM stdin;
 -- Data for Name: cars_drivers; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.cars_drivers (car_id, driver_id) FROM stdin;
+COPY public.cars_drivers (id, car_id, driver_id) FROM stdin;
+1	1	6
+2	1	3
+3	1	16
+4	1	7
+5	1	8
+6	1	5
+7	1	16
+8	1	16
+9	2	13
+10	2	17
+11	3	13
+12	3	17
+13	3	14
+14	4	1
+15	4	7
+16	4	12
+17	4	16
+18	4	5
+19	4	1
+20	5	14
+21	5	10
+22	5	3
+23	5	1
+24	5	4
+25	5	1
+26	6	9
+27	6	15
+28	6	3
+29	6	15
+30	6	3
+31	6	1
+32	6	15
+33	6	8
+34	6	4
+35	7	1
+36	7	7
+37	7	17
+38	7	6
+39	7	1
+40	8	1
+41	8	15
+42	8	9
+43	8	16
+44	9	7
+45	9	6
+46	9	14
+47	9	8
+48	9	4
+49	9	5
+50	9	11
+51	10	4
+52	10	11
+53	10	3
+54	10	3
 \.
 
 
@@ -214,7 +266,24 @@ COPY public.cars_drivers (car_id, driver_id) FROM stdin;
 -- Data for Name: drivers; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.drivers (id, name, licence_number, deleted) FROM stdin;
+COPY public.drivers (id, name, licence_number, deleted, login, password) FROM stdin;
+3	Name 0	number1	f	login 0	password 0
+4	Name 1	number1	f	login 1	password 1
+5	Name 2	number1	f	login 2	password 2
+6	Name 3	number1	f	login 3	password 3
+7	Name 4	number1	f	login 4	password 4
+8	Name 5	number1	f	login 5	password 5
+9	Name 6	number1	f	login 6	password 6
+10	Name 7	number1	f	login 7	password 7
+11	Name 8	number1	f	login 8	password 8
+12	Name 9	number1	f	login 9	password 9
+13	Name 10	number1	f	login 10	password 10
+14	Name 11	number1	f	login 11	password 11
+15	Name 12	number1	f	login 12	password 12
+16	Name 13	number1	f	login 13	password 13
+17	Name 14	number1	f	login 14	password 14
+18	Kate	314638	t	Kate123	123
+1	Oleh	678165	t	Oleh	strongPassword
 \.
 
 
@@ -223,6 +292,21 @@ COPY public.drivers (id, name, licence_number, deleted) FROM stdin;
 --
 
 COPY public.manufacturers (id, name, country, deleted) FROM stdin;
+1	Manufacturer0	Some 0	f
+2	Manufacturer1	Some 1	f
+3	Manufacturer2	Some 2	f
+4	Manufacturer3	Some 3	f
+5	Manufacturer4	Some 4	f
+6	Manufacturer5	Some 5	f
+7	Manufacturer6	Some 6	f
+8	Manufacturer7	Some 7	f
+9	Manufacturer8	Some 8	f
+10	Manufacturer9	Some 9	f
+11	Manufacturer10	Some 10	f
+12	Manufacturer11	Some 11	f
+13	Manufacturer12	Some 12	f
+14	Manufacturer13	Some 13	f
+15	Manufacturer14	Some 14	f
 \.
 
 
@@ -230,21 +314,20 @@ COPY public.manufacturers (id, name, country, deleted) FROM stdin;
 -- Name: cars_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.cars_id_seq', 1, false);
+SELECT pg_catalog.setval('public.cars_id_seq', 10, true);
 
 
 --
 -- Name: drivers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.drivers_id_seq', 1, false);
+SELECT pg_catalog.setval('public.drivers_id_seq', 18, true);
 
 
 --
 -- Name: manufacturers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.manufacturers_id_seq', 17, true);
 
 
 --
@@ -269,6 +352,13 @@ ALTER TABLE ONLY public.drivers
 
 ALTER TABLE ONLY public.manufacturers
     ADD CONSTRAINT manufacturers_pk PRIMARY KEY (id);
+
+
+--
+-- Name: drivers_login_uindex; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX drivers_login_uindex ON public.drivers USING btree (login);
 
 
 --
